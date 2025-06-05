8a111684c94d675cf364b7ea8fa701e72222f5f8
Miguel Rabih <rabihmiguel@gmail.com>
2025-06-05 13:27:28 -0500

    service fundacion

--- src/main/java/com/app/adoptwithlove/service/AdopcionService.java
+++ src/main/java/com/app/adoptwithlove/service/AdopcionService.java
@@ -1,42 +1,50 @@
 package com.app.adoptwithlove.service;
+
 import com.app.adoptwithlove.entity.Adopcion;
 import com.app.adoptwithlove.repository.AdopcionRepository;
 import com.app.adoptwithlove.service.Dao.Idao;
 
 import jakarta.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public class AdopcionService implements Idao<Adopcion, Long>{
+@Service
+public class AdopcionService implements Idao<Adopcion, Long> {
+
     @Autowired
-    private AdopcionRepository Adopcion;
+    private AdopcionRepository adopcionRepository;
+
     @Override
     public List<Adopcion> getAll() {
-        return Adopcion.findAll();
+        return adopcionRepository.findAll();
     }
 
     @Override
     public Adopcion getById(Long id) {
-        return Adopcion.findById(id).orElse(null);
+        return adopcionRepository.findById(id).orElse(null);
     }
 
     @Transactional
     @Override
-    public Adopcion create(Adopcion entity){
-        return Adopcion.save(entity);
+    public Adopcion create(Adopcion entity) {
+        return adopcionRepository.save(entity);
     }
 
     @Transactional
     @Override
-    public Adopcion update(Long id, Adopcion entity){
-        return Adopcion.save(entity);
+    public Adopcion update(Long id, Adopcion entity) {
+        if (adopcionRepository.existsById(id)) {
+            entity.setId(id); // Asegurar que el ID no cambie
+            return adopcionRepository.save(entity);
+        }
+        return null;
     }
 
     @Transactional
     @Override
-    public void delete(Long id){
-        Adopcion.deleteById(id);
+    public void delete(Long id) {
+        adopcionRepository.deleteById(id);
     }
 }
